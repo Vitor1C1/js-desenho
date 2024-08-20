7890fc572bb41a3e14b872f498b758d5c9d486cf
Vitor1C1 <00001115062931sp@al.educacao.sp.gov.br>
2024-08-20 13:18:18 -0300

    adicionando js e css

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+    background("black");
+  }
+  
+  function draw() {
+    stroke("gold");
+  fill("red");
+    if(mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 45);
+  }
+  }
